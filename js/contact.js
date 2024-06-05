1bb6e7bf61130460bc0df1627e9c9a9ff1589fe9
Alejandro Di Stefano <addistefano@live.com.ar>
2024-06-05 17:50:14 -0300

    After Class 03

--- js/contact.js
+++ js/contact.js
@@ -13,21 +13,19 @@ const textContact = document.createElement('h4');
 // }
 mainContact.appendChild(divContact);
 
-miFormulario.addEventListener('submit', (e) => {
-    e.preventDefault();
-    divContact.appendChild(textContact);   
-    textContact.innerHTML = `La Edad de ${nombreForm.value} es: ${edadForm.value} años.`;
-})
-
-// miFormulario.addEventListener("submit", validarFormulario);
-
-// function validarFormulario(e){
-//     //Cancelamos el comportamiento del evento
+// miFormulario.addEventListener('submit', (e) => {
 //     e.preventDefault();
-//     //Obtenemos el elemento desde el cual se disparó el evento
-//     let formulario = e.target
-//     //Obtengo el valor del primero hijo <input type="text">
-//     console.log(formulario.children[0].value); 
-//     //Obtengo el valor del segundo hijo <input type="number"> 
-//     console.log(formulario.children[1].value);  
-// }
+//     divContact.appendChild(textContact);
+//     textContact.innerHTML = `La Edad de ${nombreForm.value} es de: ${edadForm.value} años.`;
+// })
+
+miFormulario.addEventListener("submit", validarFormulario);
+
+function validarFormulario(e) {
+    e.preventDefault();
+    let formulario = e.target
+    divContact.appendChild(textContact);
+    textContact.innerHTML = `La Edad de ${formulario.children[1].value} es 
+                                de: ${formulario.children[3].value} años.`;
+    
+}
