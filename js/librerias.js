fe1c9cef59bca5bcd0ca963cbec8126f279d8980
Alejandro Di Stefano <addistefano@live.com.ar>
2024-06-19 18:55:22 -0300

    Ultimo After Class

--- js/librerias.js
+++ js/librerias.js
@@ -2,20 +2,19 @@ const boton = document.getElementById('boton');
 
 boton.addEventListener('click', () => {
     // Usando Sweet Alert
-    // Swal.fire({
-    //     title: "Me hicieron Click.!",
-    //     text: "Y ahora que hacemos ??? ",
-    //     // icon: "info",
-    //     imageUrl: "/img/logo.png",
-    //     imageHeight: 200,
-    //     imageAlt: "CoderHouse",
-    //     // icon: "success"
-    //     // icon: "error"
-    //     // icon: "warning"
-    //     // icon: "question"
-    //     footer: '<a href="https://coderhouse.com">Llevame a Coder</a>',
-
-    // });
+    Swal.fire({
+        title: "Me hicieron Click.!",
+        text: "Y ahora que hacemos ??? ",
+        icon: "info",
+        imageUrl: "/img/logo.png",
+        imageHeight: 200,
+        imageAlt: "CoderHouse",
+        // icon: "success"
+        // icon: "error"
+        // icon: "warning"
+        // icon: "question"
+        footer: '<a href="https://coderhouse.com">Llevame a Coder</a>',       
+    });
 
     // Swal.fire({
     //     title: "Quiere finalizar la Compra?",
@@ -51,54 +50,54 @@ boton.addEventListener('click', () => {
 
     //     }).showToast();
 
-    Toastify(
-        {
-            text: "El usuario esta OnLine.!",
-            className: "info",
-            // destination: "https://github.com/apvarun/toastify-js",
-            // newWindow: true,
-            onClick: function () {
-                Toastify({
-                    text: "El usuario esta OnLine.!",
-                    position: "right",
-                    gravity: "bottom",
-                    duration: 3000, /// Milisegundos > Equivale a 3seg
-                    offset: {
-                        x: 150, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
-                        y: 150 // vertical axis - can be a number or a string indicating unity. eg: '2em'
-                    },
-                }).showToast()
-            },
-            gravity: "top",
-            avatar: "/img/logo.png",
-            position: "right",
-            close: true,
-            style: {
-                background: "linear-gradient(to right, #12000b, #96c93d)",
-            }
-        }
-    ).showToast();
+    // Toastify(
+    //     {
+    //         text: "El usuario esta OnLine.!",
+    //         className: "info",
+    //         // destination: "https://github.com/apvarun/toastify-js",
+    //         // newWindow: true,
+    //         onClick: function () {
+    //             Toastify({
+    //                 text: "El usuario esta OnLine.!",
+    //                 position: "right",
+    //                 gravity: "bottom",
+    //                 duration: 3000, /// Milisegundos > Equivale a 3seg
+    //                 offset: {
+    //                     x: 150, // horizontal axis - can be a number or a string indicating unity. eg: '2em'
+    //                     y: 150 // vertical axis - can be a number or a string indicating unity. eg: '2em'
+    //                 },
+    //             }).showToast()
+    //         },
+    //         gravity: "top",
+    //         avatar: "/img/logo.png",
+    //         position: "right",
+    //         close: true,
+    //         style: {
+    //             background: "linear-gradient(to right, #12000b, #96c93d)",
+    //         }
+    //     }
+    // ).showToast();
 })
 
 
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal',
-    loop: true,
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     direction: 'horizontal',
+//     loop: true,
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-    },
+//     // If we need pagination
+//     pagination: {
+//         el: '.swiper-pagination',
+//     },
 
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
 
-    // And if we need scrollbar
-    scrollbar: {
-        el: '.swiper-scrollbar',
-    },
-});
+//     // And if we need scrollbar
+//     scrollbar: {
+//         el: '.swiper-scrollbar',
+//     },
+// });
